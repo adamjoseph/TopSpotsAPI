6f7b5dfcba8170e7e96a374bbb2788d0f0c68322
adamjoseph <amjoseph93@gmail.com>
2017-02-03 13:35:56 -0800

    First commit

---
+++ TopSpotsAPI/TopSpotsAPI/Controllers/TopSpotsController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TopSpotsAPI.Models;
+
+namespace TopSpotsAPI.Controllers
+{
+    public class TopSpotsController : ApiController
+    {
+        // GET: api/TopSpots
+        public IEnumerable<TopSpot> Get()
+        {
+            return JsonConvert.DeserializeObject<List<TopSpot>>(File.ReadAllText(@"C:\Users\Adam\Dev\TopSpotsApi\TopSpotsAPI\TopSpotsAPI\App_Start\TopSpots.json"));          
+        }
+
+        // GET: api/TopSpots/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TopSpots
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TopSpots/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TopSpots/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
